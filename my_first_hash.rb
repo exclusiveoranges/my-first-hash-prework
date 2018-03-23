7f33d026d283f2f76ae63da53973d16cbad89ff9
Ryan O'Toole <otoolerj@gmail.com>
2018-03-23 18:47:19 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,7 +7,7 @@ end
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
-  
+
 
 
 
@@ -31,8 +31,8 @@ def adding
                   "oil paintings" => 3
                  }
 
-  shipping_manifest["muskets"] = "2"
-  shipping_manifest["gun powder"] = "4"
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   # add 2 muskets to the shipping_manifest hash below
 
 
